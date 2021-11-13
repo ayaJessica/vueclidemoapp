3b4ebfe76253990d1add5d4000a6f273f41e0407
ayaJessica <fzx0904@163.com>
2021-11-13 18:08:46 +0800

    fix: 地址

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to aoping's blog"/>
+    <HelloWorld msg="Welcome to fangfang's blog"/>
   </div>
 </template>
 
